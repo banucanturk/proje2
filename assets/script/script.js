af4656a468336b485a02033dcf449a9943cb9341
Mert OKAN <57856404+Elitesmert@users.noreply.github.com>
2023-04-16 20:49:53 +0300

    image eklenicek

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,5 +1,8 @@
 /* Chart Bar Desktop Start */
 var chartDesktop = document.getElementById('chartDesktop').getContext('2d');
+var img = new Image();
+img.src = 'path/to/image.png';
+const fillPattern = chartDesktop.createPattern(img, 'repeat');
 
 new Chart(chartDesktop, {
     type: 'bar',
@@ -14,7 +17,7 @@ new Chart(chartDesktop, {
         }, {
             label: 'infrastructures',
             data: [null, 78, null, 20, null, null, null, null, null, null, 24],
-            backgroundColor: '#135dcb',
+            backgroundColor: fillPattern,
             borderRadius: 2,
         }]
     },
@@ -30,6 +33,13 @@ new Chart(chartDesktop, {
                     ctx.ticks.push({ value: 75, label: '75%' });
                     ctx.ticks.push({ value: 100, label: '100%' });
                 }
+            }
+        },
+        // Draw the image onto the canvas element after the chart is rendered
+        draw: function (chart) {
+            if (chart.config.type === 'bar') {
+                var ctx = chart.chart.ctx;
+                chartDesktop.drawImage(img, 0, 0, img.width, img.height, 0, 0, 30, 30);
             }
         }
     }
@@ -71,4 +81,5 @@ new Chart(chartMobile, {
             }
         }
     }
+
 });
